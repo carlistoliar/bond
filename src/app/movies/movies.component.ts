64747a113b44b93cf8bb68767d35eb5ab4c79bb6
carli stoliar <carli@carlis-MacBook-Pro.local>
2017-08-23 10:51:04 -0700

    added code and a bit od style to movies

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonDataService} from '../json-data.service';
 
 @Component({
   selector: 'app-movies',
-  templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  templateUrl: './movies.component.html'
 })
 export class MoviesComponent implements OnInit {
+  //
+  movies = [];
+  moviesUrl = './assets/data/movie_metadata.json';
+  searchTerm = null;
+  searchRating = null;
+  ratings = [
+    {'rating': 'G', 'description' : 'General Audiences'},
+    {'rating': 'PG', 'description' : 'Parental Guidance Suggested'},
+    {'rating': 'PG-13', 'description' : 'Parents Strongly Cautioned'},
+    {'rating': 'R', 'description' : 'Restricted'},
+    {'rating': 'NC-17', 'description' : 'Adults Only'},
+    {'rating': 'NR', 'description' : 'Not Yet Rated'},
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  resetSearch(event) {
+    this.searchTerm = null;
+    this.searchRating = null;
   }
-
+  //
+  constructor(private dataService: JsonDataService) { }
+  //
+  ngOnInit() {
+    this.dataService.getMovies(this.moviesUrl).subscribe((data) =>  this.movies = data);
+  }
 }
